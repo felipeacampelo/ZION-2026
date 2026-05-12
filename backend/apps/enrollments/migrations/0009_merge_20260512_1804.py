c78d415a5d4e8dcf12b9e5c80f33eb30c5f4bcee
Felipe Campelo <felipeaquinocamp@gmail.com>
2026-05-12 18:04:41 -0300

    Add merge migration for enrollments 0008 conflict

---
+++ backend/apps/enrollments/migrations/0009_merge_20260512_1804.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('enrollments', '0008_coupon_payment_restrictions_and_global_toggle'),
+        ('enrollments', '0008_email_templates_and_campaigns'),
+    ]
+
+    operations = []
